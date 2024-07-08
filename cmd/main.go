314a35caa42d9d52dbebe501786da03efda34014
erykksc <48735008+erykksc@users.noreply.github.com>
2024-07-09 01:13:12 +0200

    Add log/slog

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"log"
+	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
@@ -16,6 +18,7 @@ var (
 	Token     string
 	UserID    string
 	Separator string
+	Verbose   bool
 )
 var sc = make(chan os.Signal, 1)
 
@@ -23,17 +26,24 @@ func init() {
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.StringVar(&UserID, "u", "", "User's discord ID")
 	flag.StringVar(&Separator, "s", ":::", "Separator between message and emoji")
+	flag.BoolVar(&Verbose, "v", false, "Sets logging level to Debug")
 	flag.Parse()
 }
 
 var unresolvedMsgs = make(map[string]string)
 
 func main() {
+	logOptions := slog.HandlerOptions{}
+	if Verbose {
+		logOptions.Level = slog.LevelDebug
+	}
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &logOptions))
+	slog.SetDefault(logger)
+
 	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
-		fmt.Println("error creating Discord session,", err)
-		return
+		log.Fatalf("error creating Discord session: %s", err)
 	}
 
 	// Register the messageCreate func as a callback for MessageCreate events.
@@ -51,14 +61,13 @@ func main() {
 	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error opening connection: %s\n", err)
-		return
+		log.Fatalf("error opening connection: %s", err)
 	}
 	defer dg.Close()
 
 	channel, err := dg.UserChannelCreate(UserID)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error creating DM channel: %s\n", err)
+		log.Fatalf("error creating DM channel: %s", err)
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -66,13 +75,14 @@ func main() {
 		line := scanner.Text()
 		m, err := dg.ChannelMessageSend(channel.ID, line)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error sending message: %s\n", err)
+			log.Fatalf("error sending message: %s", err)
 		}
 
 		unresolvedMsgs[m.ID] = line
 	}
 
 	// Wait here until CTRL-C or other term signal is received.
+	slog.Debug("Bot is now running.  Press CTRL-C to exit.")
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 }
@@ -85,20 +95,20 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// We create the private channel with the user who sent the message.
 	channel, err := s.UserChannelCreate(m.Author.ID)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error creating channel: %s\n", err)
+		slog.Error("error creating channel", "error", err)
 		return
 	}
 	// Then we send the message through the channel we created.
 	_, err = s.ChannelMessageSend(channel.ID, "Pong!")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error sending DM message: %s\n", err)
+		slog.Error("error sending DM message", "error", err)
 	}
 }
 
 func reactionAdd(s *discordgo.Session, r *discordgo.MessageReactionAdd) {
 	msg, ok := unresolvedMsgs[r.MessageID]
 	if !ok {
-		fmt.Fprintf(os.Stderr, "Skipping message %s: not found in unresolved messages\n", r.MessageID)
+		slog.Info("Message not found in unresolved messages, skipping", "messageID", r.MessageID)
 		return
 	}
 
